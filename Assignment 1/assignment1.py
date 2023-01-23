64ec23cf48dc8235a508e44594c016be8c89bd77
moeongit <malrobiay04@gmail.com>
2023-01-23 02:43:39 -0700

    init

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -1,65 +1,70 @@
-def parse_players(file_name):
+import codecs
+
+def parse_players(filepath):
     players = []
-    with open(file_name, "r", encoding="utf-8") as f:
-        data = f.readlines()
-        for line in data:
+    with codecs.open(filepath, 'r', 'utf-8') as f:
+        for line in f:
             fields = line.strip().split(";")
-            player = {
-                'Country': fields[0],
-                'Position': fields[1],
-                'Number': fields[2],
-                'Name': fields[3],
-                'Birthday': fields[4],
-                'Age': fields[5]
-            }
-            players.append(player)
+            country_number = fields[0]
+            position = fields[1]
+            name = fields[2]
+            dob, age = fields[3].split(" (")
+            age = age.strip("aged ").strip(")")
+            number = country_number[-2:]
+            country = country_number[:-3]
+            players.append({
+                "country": country,
+                "number": number,
+                "position": position,
+                "name": name,
+                "dob": dob,
+                "age": age
+            })
     return players
 
-def parse_matches(file_name):
+def parse_matches(filepath):
     matches = []
-    with open(file_name, "r", encoding="utf-8") as f:
-        data = f.readlines()
-        for line in data:
+    with codecs.open(filepath, 'r', 'utf-8') as f:
+        for line in f:
             fields = line.strip().split(";")
-            group = fields[0]
-            team1 = fields[1]
-            team2 = fields[2]
-            score1 = []
-            score2 = []
-            if fields[3] != "()()":
-                score1 = [int(x) if x[0]!="X" else int(x[1:]) for x in fields[3][1:-1].split(",")]
-                score2 = [int(x) if x[0]!="X" else int(x[1:]) for x in fields[4][:-1].split(",")]
-            date = fields[5]
-            match = {
-                'Group': group,
-                'Team1': team1,
-                'Team2': team2,
-                'Score1': score1,
-                'Score2': score2,
-                'Date': date
-            }
-            matches.append(match)
+            if fields[0] and fields[1] and fields[2] and fields[3] and fields[4]:
+                group = fields[0]
+                team1 = fields[1]
+                team2 = fields[2]
+                scores = fields[3]
+                date = fields[4]
+                team1_scores = scores.split(")(")[0].strip("(")
+                team2_scores = scores.split(")(")[1].strip(")")
+                matches.append({
+                    "group": group,
+                    "team1": team1,
+                    "team2": team2,
+                    "team1_scores": team1_scores,
+                    "team2_scores": team2_scores,
+                    "date": date
+                })
     return matches
 
-def parse_yellowcards(file_name):
-    yellow_cards = []
-    with open(file_name, "r", encoding="utf-8") as f:
-        data = f.readlines()
-        for line in data:
+def parse_cards(filepath):
+    cards = []
+    with codecs.open(filepath, 'r', 'utf-8') as f:
+        for i, line in enumerate(f):
             fields = line.strip().split(";")
-            match = fields[0]
-            team = fields[1]
-            player_num = int(fields[2])
-            player_name = fields[3]
-            yellow_card = {
-                'Match': match,
-                'Team': team,
-                'Player_Number': player_num,
-                'Player_Name': player_name
-            }
-            yellow_cards.append(yellow_card)
-    return yellow_cards
+            if len(fields) == 5:
+                match, country, name, card_type, time = fields
+                cards.append({
+                    "match": match,
+                    "country": country,
+                    "name": name,
+                    "type": card_type,
+                    "time": time
+                })
+
+    return cards
 
 players = parse_players("WC22Footballers.txt")
 matches = parse_matches("WC22GroupMatches.txt")
-yellow_cards = parse_yellowcards("WC22YellowCards.txt")
+cards = parse_cards("WC22-YellowCards.txt")
+print(players)
+print(matches)
+print(cards)
