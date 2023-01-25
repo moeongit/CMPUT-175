9be5238fdfb3ac4e2ba3ee97afac04d2f1a56547
moeongit <malrobiay04@gmail.com>
2023-01-24 22:08:46 -0700

    init

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -5,13 +5,13 @@ def players(filename):
     with codecs.open(filename, 'r', 'utf-8') as file:
         for line in file:
             stats = line.strip().split(";")
-            country_number = stats[0]
+            country_number = stats[0].split(" ")
+            country = country_number[0]
+            number = country_number[1]
             position = stats[1]
             name = stats[2]
             dob, age = stats[3].split(" (")
             age = age.strip("aged ").strip(")")
-            number = country_number[-2:]
-            country = country_number[:-3]
             players.append({
                 "country": country,
                 "number": number,
@@ -21,6 +21,7 @@ def players(filename):
                 "age": age
             })
     return players
+
 
 def matches(filename):
     matches = []
@@ -59,10 +60,9 @@ def cards(filename):
                     "type": card_type,
                     "time": time
                 })
-
     return cards
 
-def write_groups(filepath, matches):
+def write_groups(filename, matches):
     groups = {}
     for match in matches:
         group = match["group"]
@@ -72,90 +72,88 @@ def write_groups(filepath, matches):
             groups[group] = set()
         groups[group].add(team1)
         groups[group].add(team2)
-    with open(filepath, "w") as f:
+    with open(filename, "w") as file:
         for group, countries in sorted(groups.items()):
-            f.write("Group {}\n".format(group))
+            file.write("Group {}\n".format(group))
             for country in sorted(countries):
-                f.write("{}\n".format(country))
-            f.write("\n")
+                file.write("{}\n".format(country))
+            file.write("\n")
 
-def knockout_stage(filepath, matches, cards):
+def average_age(players_func):
     teams = {}
-    for match in matches:
-        team1 = match["team1"]
-        team2 = match["team2"]
-        score1 = match["team1_scores"]
-        score2 = match["team2_scores"]
-        if team1 not in teams:
-            teams[team1] = {"played": 0, "wins": 0, "draws": 0, "losses": 0, "goals_for": 0, "goals_against": 0, "points": 0, "yellow_cards": 0, "red_cards": 0}
-        if team2 not in teams:
-            teams[team2] = {"played": 0, "wins": 0, "draws": 0, "losses": 0, "goals_for": 0, "goals_against": 0, "points": 0, "yellow_cards": 0, "red_cards": 0}
-        if match["team2_scores"]:
-            score2 = list(map(int, match["team2_scores"].split(',')))
-        else:
-            print("Invalid score for team 2")
-            return
-
-        score1 = list(map(int, match["team1_scores"].split(',')))
-        score2 = list(map(int, match["team2_scores"].split(',')))
-        # for s in score1:
-        #     teams[team1]["goals_for"] += s
-        # for s in score2:
-        #     teams[team2]["goals_for"] += s
-
-        teams[team1]["goals_for"] += sum(score1)
-        teams[team1]["goals_against"] += sum(score2)
-        teams[team2]["goals_for"] += sum(score2)
-        teams[team2]["goals_against"] += sum(score1)
-
-        if score1 > score2:
-            teams[team1]["wins"] += 1
-            teams[team2]["losses"] += 1
-        elif score1 < score2:
-            teams[team1]["losses"] += 1
-            teams[team2]["wins"] += 1
-        else:
-            teams[team1]["draws"] += 1
-            teams[team2]["draws"] += 1
-    for card in cards:
-        fields = card.strip().split(';')
-        match, team, player, card_type, time = fields
+    for player in players_func:
+        team = player["country"]
+        age = int(player["age"])
         if team not in teams:
-            teams[team] = {'played': 0, 'wins': 0, 'draws': 0, 'losses': 0, 'goals_for': 0, 'goals_against': 0, 'points': 0, 'yellow_cards': 0, 'red_cards': 0}
-        if card_type == 'Y':
-            teams[team]['yellow_cards'] += 1
-        else:
-            teams[team]['red_cards'] += 1
-        #calculating points
-        for team in teams:
-            wins = teams[team]['wins']
-            draws = teams[team]['draws']
-            teams[team]['points'] = wins * 3 + draws
-        #sorting teams alphabetically
-        sorted_teams = dict(sorted(teams.items(), key=lambda item: item[0]))
-        #calculating goal difference
-        for team in sorted_teams:
-            goal_diff = teams[team]['goals_for'] - teams[team]['goals_against']
-            teams[team]['goal_diff'] = goal_diff
-        #sorting teams by points, goal_difference and yellow cards
-        sorted_teams = dict(sorted(teams.items(), key=lambda item: (item[1]['points'], item[1]['goal_diff'], item[1]['yellow_cards']), reverse=True))
-        #writing the output to the console and file
-        with open(filepath, 'w', 'utf-8') as f:
-            for team in sorted_teams:
-                points = teams[team]['points']
-                goal_diff = teams[team]['goal_diff']
-                yellow_cards = teams[team]['yellow_cards']
-                red_cards = teams[team]['red_cards']
-                f.write('{:<12}'.format(team) + '{:<3}'.format(str(points)) + 'pts' + '\n')
-                print('{:<12}'.format(team) + '{:<3}'.format(str(points)) + 'pts')
+            teams[team] = {"players": 0, "age": 0}
+        teams[team]["players"] += 1
+        teams[team]["age"] += age
+    for team in teams.keys():
+        teams[team]["average_age"] = round(teams[team]["age"] / teams[team]["players"], 2)
+    with open("ages.txt", "w", encoding="utf-8") as f:
+        total_players = 0
+        total_age = 0
+        for team, values in sorted(teams.items()):
+            players = values["players"]
+            age = values["average_age"]
+            total_players += players
+            total_age += age * players
+            f.write("{:<12}".format(team) + "{:.2f}".format(age) + " years\n")
+            # print("{:<12}".format(team) + "{:.2f}".format(age) + " years")
+        f.write("\nAverage Overall " + "{:.2f}".format(total_age/total_players) + " years")
+        print("\nAverage Overall " + "{:.2f}".format(total_age/total_players) + " years")
+
+def histogram(players_func):
+    ages = {}
+    for player in players_func:
+        age = int(player["age"])
+        if age not in ages:
+            ages[age] = 0
+        ages[age] += 1
+    with open("histogram.txt", "w", encoding="utf-8") as f:
+        for age in range(18, 41):
+            if age in ages:
+                stars = round(ages[age]/5)
+                if stars == 0:
+                    stars = 1
+                f.write("{} years ({:2d}){}\n".format(age, ages[age],'*' * stars))
+                print("{} years ({:2d}){}".format(age, ages[age], '*' * stars))
+
+def most_yellow_cards(cards):
+    match_card_count = {}
+    for card in cards:
+        match = card["match"]
+        country = card["country"]
+        if match not in match_card_count:
+            match_card_count[match] = {}
+        if country not in match_card_count[match]:
+            match_card_count[match][country] = 0
+        match_card_count[match][country] += 1
+    max_cards = 0
+    max_match = ""
+    for match, card_count in match_card_count.items():
+        if sum(card_count.values()) > max_cards:
+            max_cards = sum(card_count.values())
+            max_match = match
+    match_name = max_match.split("-")
+    match_name = " vs ".join(match_name)
+    with open("yellow.txt", "w") as file:
+        file.write(match_name + "\n")
+        for country, count in match_card_count[max_match].items():
+            file.write(f"{country}: {count} YC\n")
+        print(match_name + "\n")
+        for country, count in match_card_count[max_match].items():
+            print(f"{country}: {count} YC")
 
 def main():
     players_func = players("WC22Footballers.txt")
     matches_func = matches("WC22GroupMatches.txt")
     cards_func = cards("WC22-YellowCards.txt")
     groups = write_groups("groups.txt", matches_func)
-    knockout = knockout_stage("knockout.txt", matches_func, cards_func)
-    print(players_func)
-    print(matches_func)
-    print(cards_func)
+    ages = average_age(players_func)
+    stars = histogram(players_func)
+    most_yellow_cards(cards_func)
+    # print(players_func)
+    # print(matches_func)
+    # print(cards_func)
 main()
