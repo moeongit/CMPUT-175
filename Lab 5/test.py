78d8d88b332028c5dae0aaaa2426c6deb04286b8
moealrobiay <mohammedalrobiay12@gmail.com>
2023-02-14 14:12:18 -0700

    init

---
+++ Lab 5/test.py
@@ -0,0 +1,30 @@
+from stack import Stack
+
+
+import time
+
+class Stack:
+    
+    def __init__(self, y):
+        y = self.y
+
+
+
+    def peek(fwd, back, constant):
+
+        speed = {"fwd":fwd}
+
+
+
+
+    def main():
+        
+        for i in range (1, 100):
+            fwd = peek[i] + 1
+            back = peek[i] - 1
+            constant = peek[i]
+            peek(fwd, back, constant)
+
+
+if __name__ == '__main__':
+    main()
