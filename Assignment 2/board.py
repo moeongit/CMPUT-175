6874d0182de7a5ea9ddd8f458d7cafcc3d3f2070
moeongit <malrobiay04@gmail.com>
2023-03-09 21:20:32 -0700

    init

--- Assignment 2/board.py
+++ Assignment 2/board.py
@@ -36,33 +36,34 @@ class Board:
         return self.board
     
     def __str__(self):
-        column_header = '        ' + '     '.join(Board.columns)
-        row_separator = '  +---' * self.width + '  +'
+        column_header = '      ' + '   '.join(Board.columns)
+        row_separator = '  +-' + '-+-'.join(['-'  for i in range(self.width)]) + '-+'
+
         output = [column_header, row_separator]
         for i in range(self.height):
             row = f"{i+1} |"
             for j in range(self.width):
                 stack = self.board[i][j]
                 if stack.is_empty():
-                    row += '     |'
+                    row += '   |'
                 else:
                     top_goat = stack.peek()
                     if isinstance(top_goat, Goat):
-                        row += f"  {top_goat.color[0]}  |"
+                        row += f" {top_goat.color[0]} |"
                     else:
-                        row += '  X  |'
+                        row += ' X |'
             output.append(row)
             output.append(row_separator)
         return '\n'.join(output)
 
 
-# board = Board(9, 6, [(3, 'B')])
+board = Board(9, 6, [(3, 'B')])
 
-# # add 3 goats to the board
-# board.get_board()[1][1].push(Goat('WHITE', 2, 'B'))
+# add 3 goats to the board
+board.get_board()[1][1].push(Goat('WHITE', 2, 'B'))
 
-# board.get_board()[3][3].push(Goat('BLACK', 2, 'J'))
-# # board.get_board()[5][5].push(Goat('brown'))
+board.get_board()[3][3].push(Goat('BLACK', 2, 'J'))
+board.get_board()[5][5].push(Goat('RED', 2, 'I'))
 
-# # print the board
-# print(board)
+# print the board
+print(board)
